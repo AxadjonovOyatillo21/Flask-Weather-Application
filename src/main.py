007a291f71fe546feab666493c32fe75b1b48b49
AxadjonovOyatillo21 <flaskdev21@gmail.com>
2021-02-19 16:25:37 +0500

    first commit

--- src/main.py
+++ src/main.py
@@ -52,3 +52,15 @@ def weather():
     }
 
 	return render_template('index.html', data = data) 
+
+
+@app.errorhandler(404)
+def internal_server_error(e):
+
+	return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	
+	return render_template('errors/500.html'), 500
